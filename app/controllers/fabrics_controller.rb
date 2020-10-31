aa7306c93bb2cc2f794e1a15745999e14e77040d
Chris LaBarge <chrislabargedev@gmail.com>
2020-10-31 12:53:36 -0400

    add fabrics

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -1,3 +1,14 @@
 class FabricsController < ApplicationController
-  def index; end
+  def index
+    @collection = (0...10).to_a.map do |index|
+      name = "fabric_#{index}"
+      date = Time.now.send("-", 5.days)
+
+      Fabric.new(
+        name: name,
+        image_url: "/#{name}.jpg",
+        created_at: date,
+      )
+    end
+  end
 end
---
+++ app/models/fabric.rb
@@ -0,0 +1,5 @@
+class Fabric
+  include ActiveModel::Model
+
+  attr_accessor :name, :image_url, :created_at
+end
